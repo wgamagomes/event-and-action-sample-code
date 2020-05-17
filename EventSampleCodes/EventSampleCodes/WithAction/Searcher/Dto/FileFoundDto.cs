e86a9171325cc4eba8af4a286d8fe21dbf831144
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-17 11:18:25 -0300

    [ADD] Added FileFoundDto

---
+++ EventSampleCodes/EventSampleCodes/WithAction/Searcher/Dto/FileFoundDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EventSampleCodes.Searcher.WithAction.Dto
+{
+    public class FileFoundDto 
+    {
+        public string FoundFile { get; private set; }
+        public bool CancelRequested { get; set; }
+
+        public FileFoundDto(string foundFile)
+        {
+            FoundFile = foundFile;
+        }
+    }
+}
